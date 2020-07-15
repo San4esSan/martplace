10f5895a666a4b012803ad066f3ae368af2f1a5b
San4es <mister.San4es@list.ru>
2020-07-15 14:15:42 +0400

    added featured-slider

--- app/js/main.js
+++ app/js/main.js
@@ -14,9 +14,16 @@ $(function () {
 		nextArrow: '<button type="button" class="slick-arrow click-next"><img src="./images/icons/chevron-right.png" alt="next arrow"></img></button>',
 	});
 
-
 	var mixer = mixitup('.products__inner-box');
 
+	$('.featured__slider').slick({		
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button type="button" class="slick-arrow click-prev"><img src="./images/icons/chevron-left.png" alt="prev arrow"></button>',
+		nextArrow: '<button type="button" class="slick-arrow click-next"><img src="./images/icons/chevron-right.png" alt="next arrow"></img></button>',
+	 });
 
 
 
